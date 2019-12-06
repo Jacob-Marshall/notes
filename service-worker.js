6a3806e8e717f264993b48351a1dce4e5cb5c811
Jacob-Marshall <Jacob-Marshall@users.noreply.github.com>
2019-12-06 17:35:45 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
-
+navigator.serviceWorker.ready.then(function(registration) {
+  if (!registration.pushManager) {
+    alert('No push notifications support.');
+    return false;
+  }
+  //To subscribe `push notification` from push manager
+  registration.pushManager.subscribe({
+  userVisibleOnly: true //Always show notification when received
+  })
+  .then(function (subscription) {
+  console.log('Subscribed.');
+  })
+  .catch(function (error) {
+  console.log('Subscription error: ', error);
+  });
+})
